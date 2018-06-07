4870a6d56ca74c40d199f777316f0813c03f9c8d
Van <Van@V3-ent.com>
2018-06-07 14:06:46 -0500

    added artistPass population

--- TM.js
+++ TM.js
@@ -1,13 +1,14 @@
 const tmAPI_KEY = "&apikey=S8P66L8bOZUZaq7TME2QoF5NTK2AoAVp"
 const concertURL = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&size=10'
-let movieList=$("#movieList")
+
 var genrePick="Country"
-var locationPick="Houston"
-var artistPass=[]  
 var dateRange ='&startDateTime=2018-06-05T00:00:00Z&endDateTime=2018-06-12T00:00:00Z'
+//var dateRange ='&startDateTime='+dateRangeStart+'T00:00:00Z&endDateTime='+dateRangeEnd+'T00:00:00Z'
+var locationPick=getCity()
+let movieList=$("#movieList")
+var artistPass=[]  
 var genre = genreCats[genrePick]
 var genresearch = '&classificationId='+genre
-console.log(genrePick+" is "+genre)
 var city = '&dmaId='+locationPick
 let rock=$("#rock") 
 let hip_hop=$("#hip-hop") 
@@ -23,14 +24,11 @@ rnb.click(function(){genrePick = 'RnB', buildGenre(), fetchAll()})
 function buildGenre()
     {
         genre = genreCats[genrePick]
-        console.log (genre)
         genresearch = '&classificationId='+genre
-        console.log(genrePick+" clicked")
     }
 
 function buildLocation()
     {
-        console.log(locationPick)
         var locationChoice =locationData[locationPick]
         city = '&dmaId='+locationChoice
         return city
@@ -39,9 +37,6 @@ function buildLocation()
 function fetchAll()
     {
         buildLocation()
-        console.log(genresearch)
-        console.log(city)
-        console.log(dateRange)
         let searchstring = concertURL+genresearch+city+dateRange+tmAPI_KEY
         console.log(searchstring)
         fetch(searchstring)
@@ -50,7 +45,7 @@ function fetchAll()
         {   
             for (event in json["_embedded"])
                 {
-                    console.log(json["_embedded"][event])
+//                    console.log(json["_embedded"][event])
                     return json["_embedded"][event]
                         
                 }
@@ -58,22 +53,38 @@ function fetchAll()
         .then(function(event)
             {
             movieList.html("")
+            artistPass=[]
             for (i in event)
                         {
                         let genre = event[i].classifications[0].genre.name
                         let eventTitle=event[i].name
                         let venue=event[i]._embedded.venues[0].name
                         let eventDate=event[i].dates.start.localDate
-                        let li = $("<li>").addClass("displayList");
-                        let itemTitle= $("<title>").addClass("textForm")
-                    li.append(eventTitle)
-                    li.append(" at the "+venue)
-                    li.append(" on "+eventDate)
-                    li.append(" Genre:  "+genre)
-                    movieList.append(li)
+                                                
+                        let lineup = event[i]._embedded.attractions
+                        console.log (lineup)
+                            for (index in lineup )
+                                {
+                                    let act = lineup[index].name
+                                    console.log(act)
+                                    artistPass.push(act)
+                                }
+                        buildit(genre, eventTitle, venue, eventDate )
+                        console.log(artistPass)            
+                                                
                         }
             })
     }
+
+function buildit(genre, eventTitle, venue, eventDate ){
+    let li = $("<li>").addClass("displayList");
+    let itemTitle= $("<title>").addClass("textForm")
+    li.append(eventTitle)
+    li.append(" at the "+venue)
+    li.append(" on "+eventDate)
+    li.append(" Genre:  "+genre)
+    movieList.append(li)
+}
 
 fetchAll()
  
